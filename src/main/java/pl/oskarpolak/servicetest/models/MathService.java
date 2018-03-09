e011aebfe1f98d93c7fe6b44dbeff4f5915a2daf
Oskar Polak <biuro@fivarto.pl>
2018-03-09 09:37:44 +0100

    hhh

--- src/main/java/pl/oskarpolak/servicetest/models/MathService.java
+++ src/main/java/pl/oskarpolak/servicetest/models/MathService.java
@@ -3,9 +3,12 @@ package pl.oskarpolak.servicetest.models;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MathService {
     @Setter @Getter
     private String text;
